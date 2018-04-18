ce7de9bf269bd4f12172b71b26738b4a62bb4914
Andrés Pereira de Lucena <apereira@alabs.org>
2018-04-18 14:40:46 +0200

    Adds deploy configurations

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,39 +1,14 @@
-# config valid for current version and patch releases of Capistrano
-lock "~> 3.10.1"
+# frozen_string_literal: true
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+lock "3.8.0"
 
-# Default branch is :master
-# ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
+set :application, "decidim"
+set :repo_url, "https://github.com/AjuntamentDeMataro/decidimmataro.cat"
+set :linked_files, fetch(:linked_files, []).push(".rbenv-vars")
+set :linked_dirs, fetch(:linked_dirs, []).push("log", "tmp/pids", "tmp/cache", "tmp/sockets", "vendor/bundle", "public/system", "public/cache", "public/uploads")
+set :rbenv_type, :user
+set :rbenv_ruby, "2.4.2"
+set :rbenv_path, "/home/ruby-data/.rbenv"
+set :delayed_job_workers, 1
 
-# Default deploy_to directory is /var/www/my_app_name
-# set :deploy_to, "/var/www/my_app_name"
-
-# Default value for :format is :airbrussh.
-# set :format, :airbrussh
-
-# You can configure the Airbrussh format using :format_options.
-# These are the defaults.
-# set :format_options, command_output: true, log_file: "log/capistrano.log", color: :auto, truncate: :auto
-
-# Default value for :pty is false
-# set :pty, true
-
-# Default value for :linked_files is []
-# append :linked_files, "config/database.yml", "config/secrets.yml"
-
-# Default value for linked_dirs is []
-# append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
-
-# Default value for default_env is {}
-# set :default_env, { path: "/opt/ruby/bin:$PATH" }
-
-# Default value for local_user is ENV['USER']
-# set :local_user, -> { `git config user.name`.chomp }
-
-# Default value for keep_releases is 5
-# set :keep_releases, 5
-
-# Uncomment the following to require manually verifying the host key before first deploy.
-# set :ssh_options, verify_host_key: :secure
+set :passenger_restart_with_touch, true
